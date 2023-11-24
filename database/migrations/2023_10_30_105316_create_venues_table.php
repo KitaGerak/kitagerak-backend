3d137926efed019c41516591c4dcd6b9647f1391
Kenneth Manuel Lieyanto <kennethlieyanto99@gmail.com>
2023-11-24 18:54:45 +0700

    init database migration

---
+++ database/migrations/2023_10_30_105316_create_venues_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('venues', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('address_id')->constrained();
+            $table->string('description');
+            /* $table->string('rating'); // TODO: tipe data? isinya apa? */
+            $table->string('image_url');
+            $table->timestamps();
+            $table->foreignId('venue_owner_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('venue');
+    }
+};
